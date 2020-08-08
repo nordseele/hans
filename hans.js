e057e5ea8ed6503c9728fed9274ee79ed591b457
Matthias Budde <radioanda@gmail.com>
2020-08-08 19:56:48 +0100

    add number map range

--- hans.js
+++ hans.js
@@ -76,6 +76,10 @@ udpPort.open();
 
 // ----- MIDI
 
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+    return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
 // Set up a new midi input.
 const input = new midi.Input();
 
